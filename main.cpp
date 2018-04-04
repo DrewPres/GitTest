d6208e664757aa3a30d4dba47b95f4d40904553d
DrewPres <dretpres@ut.utm.edu>
2018-04-03 21:25:30 -0500

    pushing new funciton to add

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
 using namespace std;
+void calcualte(int inNum);
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int inNum = 0;
+    cout << "Enter number" << endl;
+    cin >> inNum;
+    calcualte(inNum);
+
+}
+
+void calcualte(int inNum){
+    int total = 0;
+    for(int i = 1; i < inNum +1; i++){
+        total = i + total;
+    }
+    cout << total;
 }
